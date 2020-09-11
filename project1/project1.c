e025d1964b96036f9993e083e33bdf45e7763552
shlee4290 <shlee4290@naver.com>
2020-09-12 08:54:53 +0900

    add expression error case

--- project1/project1.c
+++ project1/project1.c
@@ -32,8 +32,10 @@ void get_token(){
 	// number value --> num
 	char next_character = getchar();
 	++character_index;
-	while (next_character == ' ' || next_character == '\t')
+	while (next_character == ' ' || next_character == '\t') {
 		next_character = getchar();
+		++character_index;
+	}
 	if (next_character == '+') {
 		token = PLUS;
 	} else if (next_character == '*') {
@@ -47,6 +49,7 @@ void get_token(){
 	} else if (isdigit(next_character)){ // 숫자인 경우
 		char number_string[NUMBER_STRING_LENGTH]; // float 데이터 형으로 바꾸기 전에 문자열 형태로 숫자를 저장해 둘 배열
 		int i = 0;
+		int dot_count = 0;
 		token = NUMBER;
 		number_string[i++] = next_character;
 		while (1) { // 여러 자리로 된 숫자를 읽는다
@@ -55,9 +58,11 @@ void get_token(){
 			if (isdigit(next_character) || next_character == '.') {
 				number_string[i++] = next_character;
 				if (next_character == '.')
+					if (++dot_count > 1) error(4); // 한 숫자 내에서 '.'이 2개 이상 나오면 에러
 					is_float = 1;
 			} else { // 읽은 문자가 실수를 이루는 문자가 아니라면
 				ungetc(next_character, stdin); // 읽었던 문자를 다시 입력 버퍼에 돌려놓는다
+				--character_index;
 				break; // 숫자 모두 읽은 것 이므로 반복 종료
 			}
 		}
@@ -119,6 +124,9 @@ void error(int i) {
 			break;
 		case 3:
 			printf("It must be empty after '^'\n");
+			break;
+		case 4:
+			printf("Invalid number format.\n");
 			break;
 	}
 	exit(1);
