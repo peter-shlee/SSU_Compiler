4908bed95cf390fef383d6a3f5c7309e59e2f3df
shlee4290 <shlee4290@naver.com>
2020-10-07 13:31:41 +0900

    test case 추가

---
+++ project5/test1-1.c
@@ -0,0 +1,4 @@
+int a, b, c = 0;
+float x = 1, y, z = 2;
+char j = 'j';
+char ch = "hello";
---
+++ project5/test2.c
@@ -0,0 +1,22 @@
+int main(void) {
+	int a = 0;
+
+	switch (a) {
+		case 1:
+			a = 0;
+			break;
+		case 2:
+			a = 0;
+			break;
+		case 3:
+			a = 0;
+			break;
+		case 4:
+			a = 0;
+			break;
+		default:
+			break;
+	}
+
+	return 0;
+}
